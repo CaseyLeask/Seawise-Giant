d470ab8af57954c511df789e0549739805f357ad
Casey Leask <casey.leask@rea-group.com>
2015-11-15 13:44:47 +1100

    Further simplified tabletop logic

--- src/main/scala/Tabletop/Tabletop.scala
+++ src/main/scala/Tabletop/Tabletop.scala
@@ -5,12 +5,9 @@ case class PlacedObject(x: Int, y: Int)
 object Tabletop {
   def dimensions = 0 to 4
 
-  val smallDimension = Tabletop.dimensions.head
-  val largeDimension = Tabletop.dimensions.last
-
   def convertPlacedObjectsToMap(placedObjects: Set[PlacedObject]): String = {
-    (smallDimension to largeDimension).map(y => {
-      (smallDimension to largeDimension).map(x => {
+    dimensions.map(y => {
+      dimensions.map(x => {
         placedObjects.contains(PlacedObject(x, y))
       }).map(isPlaced => {
         if (isPlaced) "X" else "0"
@@ -19,8 +16,8 @@ object Tabletop {
   }
 
   def validPlace(x: Int, y: Int, placedObjects: Set[PlacedObject]): Boolean = {
-    Tabletop.dimensions.contains(x) &&
-      Tabletop.dimensions.contains(y) &&
+    dimensions.contains(x) &&
+      dimensions.contains(y) &&
       !placedObjects.contains(PlacedObject(x, y))
   }
 }
